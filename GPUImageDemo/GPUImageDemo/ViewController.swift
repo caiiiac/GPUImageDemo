f02fc4faa85a2bb244e719c8af0d283f1a497567
caiiiac <caiiiac@163.com>
2017-07-03 23:00:18 +0800

    project init

---
+++ GPUImageDemo/GPUImageDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPUImageDemo
+//
+//  Created by 唐三彩 on 2017/7/3.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
