5c263768b4308cb7b9d5952ef0aeddfa26e31df6
caiiiac <caiiiac@163.com>
2017-07-03 23:18:14 +0800

    滤镜功能

--- GPUImageDemo/GPUImageDemo/ViewController.swift
+++ GPUImageDemo/GPUImageDemo/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import GPUImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    private lazy var image : UIImage = UIImage(named: "test")!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func didSelectedGaussianBlurFilter(_ sender: UIBarButtonItem) {
+        let filter = GPUImageGaussianBlurFilter()
+        // 纹理
+        filter.texelSpacingMultiplier = 5
+        imageView.image = processImage(filter)
     }
 
-
+    @IBAction func didSelectedSketchFilter(_ sender: UIBarButtonItem) {
+        let filter = GPUImageSketchFilter()
+        imageView.image = processImage(filter)
+    }
+    
+    
+    private func processImage(_ filter : GPUImageFilter) -> UIImage? {
+        // 2.1.如果是对图像进行处理GPUImagePicture
+        let picProcess = GPUImagePicture(image: image)
+        
+        // 2.2.添加需要处理的滤镜
+        picProcess?.addTarget(filter)
+        
+        // 2.3.处理图片
+        filter.useNextFrameForImageCapture()
+        picProcess?.processImage()
+        
+        // 2.4.取出最新的图片
+        return filter.imageFromCurrentFramebuffer()
+    }
 }
 
