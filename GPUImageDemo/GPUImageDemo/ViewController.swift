7965f2f92c6e6af8c77d907ca5d73236554bcfe4
caiiiac <caiiiac@163.com>
2017-07-04 16:15:56 +0800

    怀旧 复原按钮功能实现

--- GPUImageDemo/GPUImageDemo/ViewController.swift
+++ GPUImageDemo/GPUImageDemo/ViewController.swift
@@ -18,31 +18,44 @@ class ViewController: UIViewController {
 
     }
 
+    //高斯模糊
     @IBAction func didSelectedGaussianBlurFilter(_ sender: UIBarButtonItem) {
         let filter = GPUImageGaussianBlurFilter()
-        // 纹理
-        filter.texelSpacingMultiplier = 5
+        
+//        filter.texelSpacingMultiplier = 15
+        filter.blurRadiusInPixels = 5
         imageView.image = processImage(filter)
     }
 
+    //素描
     @IBAction func didSelectedSketchFilter(_ sender: UIBarButtonItem) {
         let filter = GPUImageSketchFilter()
         imageView.image = processImage(filter)
     }
     
+    //怀旧
+    @IBAction func didSelectedSepiaFilter(_ sender: UIBarButtonItem) {
+        let filter = GPUImageSepiaFilter()
+        imageView.image = processImage(filter)
+    }
+    
+    //复原
+    @IBAction func didSelectedRecovery(_ sender: UIBarButtonItem) {
+        imageView.image = image
+    }
     
     private func processImage(_ filter : GPUImageFilter) -> UIImage? {
-        // 2.1.如果是对图像进行处理GPUImagePicture
+        // 如果是对图像进行处理GPUImagePicture
         let picProcess = GPUImagePicture(image: image)
         
-        // 2.2.添加需要处理的滤镜
+        // 添加需要处理的滤镜
         picProcess?.addTarget(filter)
         
-        // 2.3.处理图片
+        // 处理图片
         filter.useNextFrameForImageCapture()
         picProcess?.processImage()
         
-        // 2.4.取出最新的图片
+        // 取出最新的图片
         return filter.imageFromCurrentFramebuffer()
     }
 }
