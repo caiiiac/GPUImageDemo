ccbcb2fdebb6aef052cb062f64e213bee5ef36dc
caiiiac <caiiiac@163.com>
2017-07-05 23:26:28 +0800

    实时视频采集

--- GPUImageDemo/GPUImageDemo/VideoCameraViewController.swift
+++ GPUImageDemo/GPUImageDemo/VideoCameraViewController.swift
@@ -7,29 +7,76 @@
 //
 
 import UIKit
+import GPUImage
+
 
 class VideoCameraViewController: UIViewController {
 
+    //MARK: - lazy
+    //视频源
+    private lazy var camera : GPUImageVideoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSessionPresetHigh, cameraPosition: .front)
+
+    //预览图层
+    private lazy var preview : GPUImageView = GPUImageView(frame: self.view.bounds)
+    
+    //初始化滤镜
+    let bilateralFilter = GPUImageBilateralFilter()     //磨皮
+    let exposureFilter = GPUImageExposureFilter()       //曝光
+    let brightnessFilter = GPUImageBrightnessFilter()   //美白
+    let satureationFilter = GPUImageSaturationFilter()  //饱和
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func setupCamera() {
+        //设置camera方向
+        camera.outputImageOrientation = .portrait
+        camera.horizontallyMirrorFrontFacingCamera = true
+        
+        //添加预览图层
+        view.insertSubview(preview, at: 0)
+        
+        //获取滤镜组
+        let filterGroup = getGroupFilters()
+        
+        
+        //设置GPUImage的响应链
+        camera.addTarget(filterGroup)
+        filterGroup.addTarget(preview)
+        
+        //开始采集视频
+        camera.startCapture()
+        
+        camera.delegate = self
     }
-    */
+    
+    //创建滤镜组
+    private func getGroupFilters() -> GPUImageFilterGroup {
+        let filterGroup = GPUImageFilterGroup()
+        
+        //设置滤镜链接关系
+        bilateralFilter.addTarget(brightnessFilter)
+        brightnessFilter.addTarget(exposureFilter)
+        exposureFilter.addTarget(satureationFilter)
+        
+        //设置group起始点 终点
+        filterGroup.initialFilters = [bilateralFilter]
+        filterGroup.terminalFilter = satureationFilter
+        
+        return filterGroup
+    }
+}
 
+
+//MARK: - GPUImageVideoCameraDelegate
+
+extension VideoCameraViewController : GPUImageVideoCameraDelegate {
+    func willOutputSampleBuffer(_ sampleBuffer: CMSampleBuffer!) {
+        print("采集到画面")
+    }
 }
